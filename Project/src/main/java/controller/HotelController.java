80a14a731e6bda45d6582605fe44bb7de50a4850
J <J@DESKTOP-RPOQ33P>
2022-10-04 13:27:09 +0900

    project

---
+++ Project/src/main/java/controller/HotelController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+
+@WebServlet("/hotel")
+public class HotelController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
