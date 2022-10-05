fc9cfe22edc14ed7653c51fb8be58fc07ebd2964
heejuu <heejuu321@gmail.com>
2022-10-05 19:34:50 +0900

    hj

--- src/main/java/service/face/ReviewService.java
+++ src/main/java/service/face/ReviewService.java
@@ -3,18 +3,23 @@ package service.face;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import dto.Hotel;
 import dto.Review;
+import dto.ReviewImage;
+import dto.Semi_User;
 
 public interface ReviewService {
 
-	public List<Review> selectAll(HttpServletRequest request);
+	public List<Review> selectAllReview(HttpServletRequest request);
+	
+	public List<List<ReviewImage>> selectAllReviewImage(HttpServletRequest request);
 	
 	public void writeReview(HttpServletRequest req);
 	
 	public Hotel selectHotelByHotelNo(HttpServletRequest request, int hotel_no);
+	
+	public List<Semi_User> selectAllReviewWriterByHotelNo(HttpServletRequest request, int hotel_no);
 	
 
 }
